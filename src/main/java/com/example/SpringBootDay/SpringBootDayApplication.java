4d6ce14272e1f1e67b07d1452dd6a1dce64e0ec6
Shivakant6387 <shivakant5875@gmail.com>
2022-12-05 23:21:34 +0530

    Added file

---
+++ src/main/java/com/example/SpringBootDay/SpringBootDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootDay/SpringBootDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
